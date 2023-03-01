71852bde3c9be8c2cdcccdf61b022609224ec448
tnwlsgk1002 <tnwlsgk1002@gmail.com>
2023-03-01 20:39:42 +0900

    :tada: init: 프로젝트 초기화
    - groovy -> kts 마이그레이션
    
    Issue #3

---
+++ app/src/main/java/com/bibbidi/myrootineclone/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bibbidi.myrootineclone.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bibbidi.myrootineclone.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
