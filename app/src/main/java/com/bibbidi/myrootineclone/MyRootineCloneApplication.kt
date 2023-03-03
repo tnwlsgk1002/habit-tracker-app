f6a1aeef85ae72a6b8221ff9d99dfec4243d03ee
tnwlsgk1002 <tnwlsgk1002@gmail.com>
2023-03-04 02:46:02 +0900

    :hammer: feat: hilt 초기 설정
    - Hilt Application Class 사용
    - MainActivity 에 종속 항목 제공
    
    Issue #3

---
+++ app/src/main/java/com/bibbidi/myrootineclone/MyRootineCloneApplication.kt
@@ -0,0 +1,7 @@
+package com.bibbidi.myrootineclone
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyRootineCloneApplication : Application()
--- app/src/main/java/com/bibbidi/myrootineclone/ui/MainActivity.kt
+++ app/src/main/java/com/bibbidi/myrootineclone/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.bibbidi.myrootineclone.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bibbidi.myrootineclone.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
