570c9f5fcf87df5fc4dcfccb1c313d2b03d6890d
tnwlsgk1002 <tnwlsgk1002@gmail.com>
2023-03-17 13:22:03 +0900

    :hammer: feat: ToolBar 및 HomeFragment 추가, statusBar 스타일 변경
    - MainActivity 에 ToolBar 추가
    - HomeFragment 추가 및 메뉴 설정
    - StatusBar 스타일 변경(반투명)
    
    Issue #9

---
+++ app/src/main/java/com/bibbidi/myrootineclone/ui/HomeFragment.kt
@@ -0,0 +1,59 @@
+package com.bibbidi.myrootineclone.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.bibbidi.myrootineclone.R
+import com.bibbidi.myrootineclone.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var viewModel: HomeViewModel
+
+    private var _binding: FragmentHomeBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.home_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_select_date -> {
+                // TODO("날짜 선택")
+                true
+            }
+            R.id.menu_set_up -> {
+                // TODO("설정")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/com/bibbidi/myrootineclone/ui/MainActivity.kt
+++ app/src/main/java/com/bibbidi/myrootineclone/ui/MainActivity.kt
@@ -14,5 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
     }
 }
