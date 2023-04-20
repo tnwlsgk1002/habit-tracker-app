c79e499a3c23bd1addd1383540201b7632a71381
tnwlsgk1002 <tnwlsgk1002@gmail.com>
2023-04-20 17:00:25 +0900

    :hammer: feat: Toolbar 의 달력 선택 시 datePicker 연결 추가
    Issue #9

--- app/src/main/java/com/bibbidi/myrootineclone/ui/HomeFragment.kt
+++ app/src/main/java/com/bibbidi/myrootineclone/ui/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
@@ -14,14 +15,23 @@ import com.bibbidi.myrootineclone.R
 import com.bibbidi.myrootineclone.databinding.FragmentHomeBinding
 import com.bibbidi.myrootineclone.ui.customview.DateState
 import com.bibbidi.myrootineclone.ui.customview.DayOfTheWeek
+import com.google.android.material.datepicker.MaterialDatePicker
 
 class HomeFragment : Fragment() {
+
+    companion object {
+        const val datePickerTag = "datePicker"
+    }
 
     private lateinit var viewModel: HomeViewModel
 
     private var _binding: FragmentHomeBinding? = null
 
     private val binding get() = _binding!!
+
+    private val datePicker = MaterialDatePicker.Builder.datePicker()
+        .setTitleText(R.string.select_date)
+        .build()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +51,7 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setUpAdapter()
+        setUpListener()
     }
 
     private fun setUpAdapter() {
@@ -116,6 +127,13 @@ class HomeFragment : Fragment() {
         }
     }
 
+    private fun setUpListener() {
+        // TODO: 날짜 선택 시 변경
+        datePicker.addOnPositiveButtonClickListener {
+            Toast.makeText(context, "$it", Toast.LENGTH_LONG).show()
+        }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.home_menu, menu)
         super.onCreateOptionsMenu(menu, inflater)
@@ -124,7 +142,7 @@ class HomeFragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.menu_select_date -> {
-                // TODO("날짜 선택")
+                showDatePicker()
                 true
             }
             R.id.menu_set_up -> {
@@ -133,6 +151,11 @@ class HomeFragment : Fragment() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun showDatePicker() {
+        // TODO: 선택한 날짜를 현재 선택된 날짜로 변경 (datePicker.selection)
+        datePicker.show(parentFragmentManager, datePickerTag)
     }
 
     override fun onDestroyView() {
