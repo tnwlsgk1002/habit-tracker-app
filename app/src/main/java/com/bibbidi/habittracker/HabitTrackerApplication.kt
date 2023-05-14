c21fef0e887a810685a4fde1dad840e34eab773a
tnwlsgk1002 <tnwlsgk1002@gmail.com>
2023-05-14 16:50:36 +0900

    :bug: fix: ThreeTen 초기화
    - Application 에서 ThreeTen 초기화
    
    Issue: #21

--- app/src/main/java/com/bibbidi/habittracker/HabitTrackerApplication.kt
+++ app/src/main/java/com/bibbidi/habittracker/HabitTrackerApplication.kt
@@ -1,7 +1,13 @@
 package com.bibbidi.habittracker
 
 import android.app.Application
+import com.jakewharton.threetenabp.AndroidThreeTen
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class HabitTrackerApplication : Application()
+class HabitTrackerApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AndroidThreeTen.init(this)
+    }
+}
