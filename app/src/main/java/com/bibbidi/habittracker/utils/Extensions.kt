0defee7649e0a20760e07ed4e7510628a06e6fee
tnwlsgk1002 <tnwlsgk1002@gmail.com>
2023-05-16 20:04:23 +0900

    :hammer: feat: 초기 습관 설정에서 Dialog 초기화 및 입력받기 구현
    - 각 Dialog 변수 초기화
    - Dialog 과 ViewModel 상호 작용 구현
    
    Issue: #19

--- app/src/main/java/com/bibbidi/habittracker/utils/Extensions.kt
+++ app/src/main/java/com/bibbidi/habittracker/utils/Extensions.kt
@@ -1,6 +1,14 @@
 package com.bibbidi.habittracker.utils
 
+import android.text.Editable
+import android.view.View
+import android.widget.EditText
+import org.threeten.bp.Duration
+import org.threeten.bp.Instant
+import org.threeten.bp.LocalDate
 import org.threeten.bp.LocalTime
+import org.threeten.bp.ZoneId
+import org.threeten.bp.ZonedDateTime
 
 fun LocalTime.toGoalTimeString(): String {
     return if (hour == 0 && minute == 0) {
@@ -11,5 +19,47 @@ fun LocalTime.toGoalTimeString(): String {
         "${hour}h"
     } else {
         "${hour}h ${minute}s"
+    }
+}
+
+fun Duration.toGoalTimeString(): String {
+    val hour = toHoursPart()
+    val minute = toMinutesPart()
+    return if (hour == 0 && minute == 0) {
+        "0s"
+    } else if (hour == 0) {
+        "${minute}s"
+    } else if (minute == 0) {
+        "${hour}h"
+    } else {
+        "${hour}h ${minute}s"
+    }
+}
+
+fun Long.asLocalDate(): LocalDate {
+    val instance = Instant.ofEpochMilli(this)
+    return instance.atZone(ZoneId.systemDefault()).toLocalDate()
+}
+
+fun LocalDate.asLong(): Long {
+    val dateTime = atStartOfDay()
+    val zoneDateTime = ZonedDateTime.of(dateTime, ZoneId.systemDefault())
+    return zoneDateTime.toInstant().toEpochMilli()
+}
+
+fun Int?.toTwoDigits(): String {
+    return String.format("%02d", this ?: 0)
+}
+
+fun Editable?.toTwoDigits(): String {
+    val number = toString().toInt()
+    return number.toTwoDigits()
+}
+
+fun EditText.toFixToTwoDigits() {
+    onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+        if (!hasFocus) {
+            setText(text.toTwoDigits())
+        }
     }
 }
