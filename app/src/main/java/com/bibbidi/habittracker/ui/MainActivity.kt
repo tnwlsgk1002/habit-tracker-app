22f9f28705c1e544c92dff0e4925b88d3e195404
tnwlsgk1002 <tnwlsgk1002@gmail.com>
2023-06-12 10:07:33 +0900

    :hammer: feat: 날짜 선택 시 toolbar 제목 변경
    - toolbar 를 Activity 에서 Fragment 로 이동
    - dateFlow 에 따라 toolbar 의 title 변경
    
    Issue: #32

--- app/src/main/java/com/bibbidi/habittracker/ui/MainActivity.kt
+++ app/src/main/java/com/bibbidi/habittracker/ui/MainActivity.kt
@@ -3,8 +3,6 @@ package com.bibbidi.habittracker.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
 import com.bibbidi.habittracker.R
 import com.bibbidi.habittracker.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,13 +17,6 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        NavigationUI.setupWithNavController(binding.toolbar, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
---
+++ app/src/main/java/com/bibbidi/habittracker/ui/binding/Toolbar.kt
@@ -0,0 +1,11 @@
+package com.bibbidi.habittracker.ui.binding
+
+import androidx.databinding.BindingAdapter
+import com.bibbidi.habittracker.R
+import com.google.android.material.appbar.MaterialToolbar
+import org.threeten.bp.LocalDate
+
+@BindingAdapter("bind:title")
+fun setTitle(view: MaterialToolbar, date: LocalDate) {
+    view.title = view.context.getString(R.string.date, date.year, date.month.value)
+}
